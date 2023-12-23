0d71d2be8edb002628087bb5214ce6e763681e90
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-23 17:31:48 -0500

    defined blueprints

---
+++ anime_web/auth.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+# defining this file as the blueprint of the application
+# defined the blueprint
+auth = Blueprint('auth', __name__)
---
+++ anime_web/views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+# defining this file as the blueprint of the application
+# defined the blueprint
+views = Blueprint('views', __name__)
+
+
