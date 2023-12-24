5fae7a16d0f3b858e491504ae834c19985728f9a
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-12-23 23:20:04 -0500

    added routes to auth file

--- anime_web/__init__.py
+++ anime_web/__init__.py
@@ -7,4 +7,13 @@ def create_app():
     # Secure the cookies session data; the secret key for the app
     app.config["SECRET_KEY"] = 'THIS IS THE SECRET KEY'
 
+
+    # importing the blueprints
+    from .views import views
+    from .auth import auth
+
+    # registering the blueprints; accessing all urls with a /
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
--- anime_web/auth.py
+++ anime_web/auth.py
@@ -3,3 +3,19 @@ from flask import Blueprint
 # defining this file as the blueprint of the application
 # defined the blueprint
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return '<p>Login</p>'
+
+
+@auth.route('/logout')
+def logout():
+    return '<p>Logout</p>'
+
+
+@auth.route('/sign-up')
+def sin_up():
+    return '<p>Sign Up</p>'
+
