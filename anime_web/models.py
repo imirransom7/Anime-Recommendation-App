08a87bd8344329eb39ced546c2d89ff2fb2ddcc4
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-03-03 07:50:22 -0500

    made images model for the anime pictures

--- anime_web/models.py
+++ anime_web/models.py
@@ -26,6 +26,11 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
 
 
+class AnimeImages(db.Model):
+    anime_id = db.Column(db.Integer, db.ForeignKey('anime_id'), nullable=False)
+    images = db.Column(db.Text)
+
+
 class AnimeGenres(db.Model):
     anime_id = db.Column(db.Integer, db.ForeignKey('anime_id'), nullable=False)
     name = db.Column(db.String(35))
