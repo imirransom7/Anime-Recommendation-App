b2b28c754e959f6c9fb15aa703ae83a78cdeed84
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-05-06 15:20:17 -0400

    changing the title page image

--- anime_web/models.py
+++ anime_web/models.py
@@ -26,50 +26,53 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
 
 
-class AnimeDataset(db.Model):
-    anime_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(70))
-    score = db.Column(db.Float)
-    type = db.Column(db.String(20))
-    # The columns where the anime is not finished show up as 'UNKNOWN'; might have to change to something else
-    episodes = db.Column(db.Integer)
-    aired = db.Column(db.String(30))
-    premiered = db.Column(db.String(20))
-    status = db.Column(db.String(20))
-    producers = db.Column(db.String(100))
-    licensors = db.Column(db.String(100))
-    studios = db.Column(db.String(100))
-    source = db.Column(db.String(20))
-    duration = db.Column(db.String(30))
-    rating = db.Column(db.String(40))
-    rank = db.Column(db.Integer)
-    popularity = db.Column(db.Integer)
-    favorites = db.Column(db.Integer)
-    scored_by = db.Column(db.Integer)
-    members = db.Column(db.Integer)
-    images_url = db.Column(db.Text)
-    genre = db.relationship('AnimeDataset', backref='anime_dataset')
-    images = db.relationship('AnimeDataset', backref='anime_dataset')
+# class AnimeDataset(db.Model):
+#     anime_id = db.Column(db.Integer, primary_key=True, unique=True)
+#     name = db.Column(db.String(70))
+#     score = db.Column(db.Float)
+#     type = db.Column(db.String(20))
+#     # The columns where the anime is not finished show up as 'UNKNOWN'; might have to change to something else
+#     episodes = db.Column(db.Integer)
+#     aired = db.Column(db.String(30))
+#     premiered = db.Column(db.String(20))
+#     status = db.Column(db.String(20))
+#     producers = db.Column(db.String(100))
+#     licensors = db.Column(db.String(100))
+#     studios = db.Column(db.String(100))
+#     source = db.Column(db.String(20))
+#     duration = db.Column(db.String(30))
+#     rating = db.Column(db.String(40))
+#     rank = db.Column(db.Integer)
+#     popularity = db.Column(db.Integer)
+#     favorites = db.Column(db.Integer)
+#     scored_by = db.Column(db.Integer)
+#     members = db.Column(db.Integer)
+#     images_url = db.Column(db.Text)
+#     # References to the AnimeDataset Model; initiating the relationship
+#     genre = db.relationship('AnimeGenres', backref='anime_dataset')
+#     images = db.relationship('AnimeImages', backref='anime_dataset')
+#
+#
+# class AnimeImages(db.Model):
+#     _id = db.Column(db.Integer, primary_key=True)
+#     anime_id = db.Column(db.Integer, db.ForeignKey('anime_dataset.anime_id'), nullable=False)
+#     images = db.Column(db.Text)
+#
+#
+# class AnimeGenres(db.Model):
+#     _id = db.Column(db.Integer, primary_key=True)
+#     anime_id = db.Column(db.Integer, db.ForeignKey('anime_dataset.anime_id'), nullable=False)
+#     name = db.Column(db.String(35))
+#
+#     # This needs to go to the AnimeDataset Model when it is made
+#     # genre = db.relationship('AnimeDataset', backref='anime')
 
 
-class AnimeImages(db.Model):
-    anime_id = db.Column(db.Integer, db.ForeignKey('anime_id'), nullable=False)
-    images = db.Column(db.Text)
-
-
-class AnimeGenres(db.Model):
-    anime_id = db.Column(db.Integer, db.ForeignKey('anime_dataset.anime_id'), nullable=False)
-    name = db.Column(db.String(35))
-
-    # This needs to go to the AnimeDataset Model when it is made
-    # genre = db.relationship('AnimeDataset', backref='anime')
-
-
-class UserDetails(db.model):
+class UserDetails(db.Model):
     # id for the users from the anime dataset (not the anime dataset mmodel)
     mal_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Strings(100))
-    gender = db.Column(db.Strings(20))
+    username = db.Column(db.String(100))
+    gender = db.Column(db.String(20))
     birthday = db.Column(db.Date)
     location = db.Column(db.Text)
     joined = db.Column(db.Text)
@@ -84,7 +87,6 @@ class UserDetails(db.model):
     rewatched = db.Column(db.Integer)
     episodes_watched = db.Column(db.Integer)
     age = db.Column(db.Integer)
-
 
 
 # class AnimeFiltered(db.Model):
